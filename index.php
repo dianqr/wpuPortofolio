6b5ccd8dee697975700a32bbbba3ba4f7138f0d9
dianqr <diansayyidah358@gamil.com>
2025-05-30 18:25:22 +0700

    subscribebtn

--- index.php
+++ index.php
@@ -113,7 +113,8 @@ $latesVideoId = $result['items']['0']['id']['videoId'];
               </div>
               <div class="col-md-8">
                 <h5><?= $channelName; ?></h5>
-                <p><?= $subscriber; ?></p>
+                <p><?= $subscriber; ?> Subscriber.</p>
+                <div class="g-ytsubscribe" data-channelid="UCzCedBCSSltI1TFd3bKyN6g" data-layout="default" data-count="default"></div>
               </div>
             </div>
             <div class="row mt-3 pb-3">
@@ -303,5 +304,6 @@ $latesVideoId = $result['items']['0']['id']['videoId'];
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
   </body>
 </html>
